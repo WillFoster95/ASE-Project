ff9d18dc781389a90ae6202528c9178d2da9ab30
WillFoster95 <w.foster5764@student.leedsbeckett.ac.uk>
2019-10-28 12:38:17 +0000

    Failiarization with Windows forms, created form, created shapes class

--- ASE-Project/Form1.Designer.cs
+++ ASE-Project/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.codeArea = new System.Windows.Forms.RichTextBox();
+            this.consoleBox = new System.Windows.Forms.RichTextBox();
+            this.singleCommandLine = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // codeArea
+            // 
+            this.codeArea.Location = new System.Drawing.Point(12, 12);
+            this.codeArea.Name = "codeArea";
+            this.codeArea.Size = new System.Drawing.Size(407, 324);
+            this.codeArea.TabIndex = 0;
+            this.codeArea.Text = "";
+            // 
+            // consoleBox
+            // 
+            this.consoleBox.Location = new System.Drawing.Point(12, 368);
+            this.consoleBox.Name = "consoleBox";
+            this.consoleBox.Size = new System.Drawing.Size(407, 70);
+            this.consoleBox.TabIndex = 2;
+            this.consoleBox.Text = "";
+            // 
+            // singleCommandLine
+            // 
+            this.singleCommandLine.Location = new System.Drawing.Point(12, 342);
+            this.singleCommandLine.Name = "singleCommandLine";
+            this.singleCommandLine.Size = new System.Drawing.Size(407, 20);
+            this.singleCommandLine.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.singleCommandLine);
+            this.Controls.Add(this.consoleBox);
+            this.Controls.Add(this.codeArea);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox codeArea;
+        private System.Windows.Forms.RichTextBox consoleBox;
+        private System.Windows.Forms.TextBox singleCommandLine;
     }
 }
 
